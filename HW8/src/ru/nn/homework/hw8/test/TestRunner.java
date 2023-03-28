c9a8c21bde41ff1a59725a3c30187a178f40d12e
Natalie <pink_mouse_@mail.ru>
2023-03-28 11:33:35 +0300

    Исправлены тесты для DiceImpl

--- HW8/src/ru/nn/homework/hw8/test/TestRunner.java
+++ HW8/src/ru/nn/homework/hw8/test/TestRunner.java
@@ -6,8 +6,6 @@ import ru.nn.homework.hw8.test.unit.GameTest;
 public class TestRunner {
     public static void main(String[] args) {
         new DiceTest().testRollReturnNumberBetweenOneAndSix();
-        new DiceTest().testRollReturnNumberLessThenOne();
-        new DiceTest().testRollReturnNumberBiggerSix();
 
         new GameTest().testForDraw();
         new GameTest().testForFirstPlayerWin();
--- HW8/src/ru/nn/homework/hw8/test/unit/DiceTest.java
+++ HW8/src/ru/nn/homework/hw8/test/unit/DiceTest.java
@@ -1,64 +1,16 @@
 package ru.nn.homework.hw8.test.unit;
 
-import ru.nn.homework.hw8.game.api.service.Dice;
-import ru.nn.homework.hw8.assertions.*;
+import ru.nn.homework.hw8.game.service.DiceImpl;
 
 public class DiceTest {
-    public void testRollReturnNumberBetweenOneAndSix() {
-        Dice dice = () -> 3;
+    private final DiceImpl dice = new DiceImpl();
 
+    public void testRollReturnNumberBetweenOneAndSix() {
         String scenario = "Тест возврата числа от 1 до 6";
         try {
             int number = dice.roll();
-            Assertions.assertEquals(3, number);
-            System.out.printf("\"%s\" passed %n", scenario);
-        } catch (Throwable e) {
-            System.err.printf("\"%s\" fails with message \"%s\" %n", scenario, e.getMessage());
-        }
-    }
-
-    public void testRollReturnNumberLessThenOne() {
-        Dice dice = () -> 0;
-
-        String scenario = "Тест возврата числа меньше 1";
-
-        try {
-            Throwable actual = null;
-            try {
-                dice.roll();
-            } catch (Throwable e) {
-                actual = e;
-            }
-
-            if (actual == null) {
-                throw new AssertionError("Given code does not throw any exception");
-            } else {
-                Assertions.assertEquals(Exception.class, actual.getClass());
-            }
-
-            System.out.printf("\"%s\" passed %n", scenario);
-        } catch (Throwable e) {
-            System.err.printf("\"%s\" fails with message \"%s\" %n", scenario, e.getMessage());
-        }
-    }
-
-    public void testRollReturnNumberBiggerSix() {
-        Dice dice = () -> 10;
-
-        String scenario = "Тест возврата числа больше 6";
-
-        try {
-            Throwable actual = null;
-            try {
-                dice.roll();
-            } catch (Throwable e) {
-                actual = e;
-            }
-
-            if (actual == null) {
-                throw new AssertionError("Given code does not throw any exception");
-            } else {
-                Assertions.assertEquals(Exception.class, actual.getClass());
+            if (number < 1 || number > 6) {
+                throw new AssertionError(String.format("Expected [1-6] = %d", number));
             }
 
             System.out.printf("\"%s\" passed %n", scenario);
